eb0b902f98eb1194b164ae870a2e9b30f5083498
Greg Lewis <glewis@eyesbeyond.com>
2019-09-17 15:51:51 -0700

    Switch ia32 specifics to be behind "#ifdef i586"
    * Sync with LinuxDebuggerLocal.c:
      * Define i586 if it isn't defined and i386 is.  This shouldn't be
        needed since -Di586 is defined on the compiler invocation.
      * Switch to using i586 as the define to look for rather than i386
        for ia32 specifics.
    
    Fixes the build of the Serviceability Agent on ia32

--- src/jdk.hotspot.agent/bsd/native/libsaproc/BsdDebuggerLocal.c
+++ src/jdk.hotspot.agent/bsd/native/libsaproc/BsdDebuggerLocal.c
@@ -30,7 +30,11 @@
 #define amd64 1
 #endif
 
-#ifdef i386
+#if defined(i386) && !defined(i586)
+#define i586 1
+#endif
+
+#ifdef i586
 #include "sun_jvm_hotspot_debugger_x86_X86ThreadContext.h"
 #endif
 
@@ -317,7 +321,7 @@ JNIEXPORT jlongArray JNICALL Java_sun_jvm_hotspot_debugger_bsd_BsdDebuggerLocal_
   }
 
 #undef NPRGREG
-#ifdef i386
+#ifdef i586
 #define NPRGREG sun_jvm_hotspot_debugger_x86_X86ThreadContext_NPRGREG
 #endif
 #ifdef amd64
@@ -339,7 +343,7 @@ JNIEXPORT jlongArray JNICALL Java_sun_jvm_hotspot_debugger_bsd_BsdDebuggerLocal_
 
 #undef REG_INDEX
 
-#ifdef i386
+#ifdef i586
 #define REG_INDEX(reg) sun_jvm_hotspot_debugger_x86_X86ThreadContext_##reg
 
   regs[REG_INDEX(GS)]  = (uintptr_t) gregs.r_gs;
@@ -358,7 +362,7 @@ JNIEXPORT jlongArray JNICALL Java_sun_jvm_hotspot_debugger_bsd_BsdDebuggerLocal_
   regs[REG_INDEX(CS)]  = (uintptr_t) gregs.r_cs;
   regs[REG_INDEX(SS)]  = (uintptr_t) gregs.r_ss;
 
-#endif /* i386 */
+#endif /* i586 */
 
 #ifdef amd64
 #define REG_INDEX(reg) sun_jvm_hotspot_debugger_amd64_AMD64ThreadContext_##reg
